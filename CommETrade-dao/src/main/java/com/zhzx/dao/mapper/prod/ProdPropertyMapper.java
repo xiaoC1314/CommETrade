9899b9fd8b3b1aaf3ffa609ed0231ce4e273819f
xiaoC1314 <ch123456>
2017-07-09 23:17:34 +0800

    增加产品属性

---
+++ CommETrade-dao/src/main/java/com/zhzx/dao/mapper/prod/ProdPropertyMapper.java
@@ -0,0 +1,13 @@
+package com.zhzx.dao.mapper.prod;
+
+import com.zhzx.dao.mapper.BaseMapper;
+
+/**
+ * ProdProperty Mapper
+ * @author xiaoC
+ *
+ */
+public interface ProdPropertyMapper<T> extends BaseMapper<T> {
+	
+	
+}
